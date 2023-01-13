2188aa41529517c5488a838a7da01414eee60923
Dario Soatto <dsoatto23@students.polytechnic.org>
2023-01-13 09:39:40 -0800

    Completed Playlist methods

--- Playlist.java
+++ Playlist.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
-/**
- * The Playlist class, which will keep track of a playlist of Song objects
- * Refer to the project description to make sure you have access to all available methods
- */
+    /**
+     * The Playlist class, which will keep track of a playlist of Song objects
+     * Refer to the project description to make sure you have access to all available methods
+     */
 public class Playlist {
     /**
      * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
@@ -14,24 +14,42 @@ public class Playlist {
 
 
 
-     /**
-      * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
-      * then use additional methods to add Songs in one-by-one
-      */
+    /**
+    * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
+    * then use additional methods to add Songs in one-by-one
+    */
     public Playlist(){
         songs = new ArrayList<Song>();
     }
 
       
 
-      /**
-       * Methods-- Remember that you need to be able to complete all of the following:
-       * Adding a song
-       * 'liking' a song
-       * Removing a specific song
-       * Examining all Songs (a String return or void print makes sense here)
-       * Examining a sublist of all liked songs
-       * Determining the total duration of all songs
-       * Removing all unliked songs from the playlist (careful with this one!)
-       */
+    /**
+     * Methods-- Remember that you need to be able to complete all of the following:
+     * Adding a song
+     * 'liking' a song
+     * Removing a specific song
+     * Examining all Songs (a String return or void print makes sense here)
+     * Examining a sublist of all liked songs
+     * Determining the total duration of all songs
+     * Removing all unliked songs from the playlist (careful with this one!)
+     */
+
+    public void addSong(Song newSong) {
+        songs.add(newSong);
+    }
+
+    public void likeUnlike(int position) {
+        (songs.get(position)).changeLike();
+    }
+
+    public void removeSong(int position) {
+        songs.remove(position);
+    }
+    
+    public void displaySongs() {
+        for(int i = 0; i < songs.size(); i++) {
+            System.out.println(songs.get(i));
+        }
+    }
 }
